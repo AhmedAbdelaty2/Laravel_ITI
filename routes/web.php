de1276516bda55e4bf6cb83655436e530941d696
ahmedabdelaty2 <ahmedabdelaty2021@gmail.com>
2022-04-17 14:50:09 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,6 @@ use App\Http\Controllers\PostController;
 |
 */
 
-
-Route::get('/', function () {
-    // return 'hello world';
-    return "view('welcome')";
-});
-
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/create/', [PostController::class, 'create'])->name('posts.create');
 Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
