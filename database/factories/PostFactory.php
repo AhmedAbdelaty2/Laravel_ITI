b1c0c198912cd19b68ef2f933dcc6c3effa93bdf
ahmedabdelaty2 <ahmedabdelaty2021@gmail.com>
2022-04-19 16:32:29 +0200

    database seeding works

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Post;
+
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+
+    protected $model = post::class;
+    
+    public function definition()
+    {
+        return [
+            "title" => $this->faker->sentence($maxNbChars = 1),
+            "description" => $this->faker->realText($maxNbChars = 50),
+            "user_id" => $this->faker->numberBetween(1,2)
+        ];
+    }
+}
